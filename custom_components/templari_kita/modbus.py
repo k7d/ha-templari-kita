69622b1eae7581179ae0968d8ad2a253fe0df1b1
Kaspars Dancis <kaspars@whimsical.com>
2024-06-11 08:58:48 +0300

    Improve error handling

--- custom_components/templari_kita/modbus.py
+++ custom_components/templari_kita/modbus.py
@@ -1,23 +1,31 @@
 from pymodbus.client import AsyncModbusTcpClient
 from pymodbus import ExceptionResponse
+import logging
 
+_LOGGER = logging.getLogger(__name__)
 
 class ClientException(Exception):
     def __init__(self, error):
         self.error = error
 
 
-async def read_register(client, address):
+async def read_register(client, address) -> int | None:
     rr = await client.read_input_registers(address, 1, slave=1)
     if rr.isError() or isinstance(rr, ExceptionResponse):
-        raise ClientException(f"read_error")
+        _LOGGER.warning(f"Modbus error while reading register {address} ({rr})")
+        return None
+    if len(rr.registers) == 0:
+        _LOGGER.warning(f"Empty Modbus response while reading register {address}")
+        return None
     return rr.registers[0]
 
 
-async def connect(host: str, port: int):
+async def connect(host: str, port: int) -> AsyncModbusTcpClient:
     client = AsyncModbusTcpClient(host=host, port=port)
     await client.connect()
     if not client.connected:
         raise ClientException("invalid_host")
-    await read_register(client, 0) # make sure there is a valid response
+    # Make sure there is a valid response:
+    if await read_register(client, 0) is None:
+        raise ClientException("invalid_host")
     return client
